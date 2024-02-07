f7e4341b545746f32f8664c51bce4e2533412f9a
Isaac Garcia <garciai0526@hotmail.com>
2024-02-06 23:48:09 -0500

    adding bus

--- lib/aws_training-stack.ts
+++ lib/aws_training-stack.ts
@@ -3,17 +3,15 @@ import * as sns from 'aws-cdk-lib/aws-sns';
 import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
+import { EventBus } from 'aws-cdk-lib/aws-events';
 
 export class AwsTrainingStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'AwsTrainingQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    // 👇 Defining our Event Bus
+    const eventBus = new EventBus(this, "eventBus", {
+      eventBusName: "isaac-event-bus",
     });
-
-    const topic = new sns.Topic(this, 'AwsTrainingTopic');
-    //ok
-    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
